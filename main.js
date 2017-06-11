5e917ed8ae3c9fddce0582f3ca69481f104046bd
Brad Broge <brbroge@gmail.com>
2017-06-11 14:38:11 -0400

    Adding core functionality, need to fix sqrt still

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 // Need to build 2 lists, one of the adjusted numbers, one of the operators
 // will only use the '^', '*', '%', and '+' operators
 // numbers will be adjusted with - (n*-1), / (1/n), and sqrt (n**.5);
+// keep track if input isNum or operator.
+
+var numb = [], oper = [], out = "", isNum = true;
+var addSqrt = false, addNeg = false, addDen = false;
 
 // Adding a bunch of listeners for all the buttons
+// WAIT: May be able to use event Delegation to only have one listener on
+// the calc-body section, and work from there.
 // let clear = document.querySelector('.clear');
-// let output = document.querySelector('.output');
 // let equal = document.querySelector('.equal');
 // let plus = document.querySelector('.plus');
 // let minus = document.querySelector('.minus');
@@ -24,10 +29,115 @@
 // let eight = document.querySelector('.eight');
 // let nine = document.querySelector('.nine');
 // let zero = document.querySelector('.zero');
-let buttons = document.querySelectorAll('button');
+
+let output = document.querySelector('.output');
+let calculator = document.querySelector('.calc-body');
+calculator.addEventListener('click', updateValues, false);
+
+function updateValues(event) {
+  // console.log('clicked');
+  let target = event.target;
+  // console.log(target);
+  if (target.matches('.number')) {
+    if (!isNum) {
+      output.value += " ";
+      isNum = true;
+    }
+    // console.log(event.target);
+    // console.log("button pressed: ", target.value);
+    output.value += event.target.value;
+  } else
+  if (target.matches('.clear')) {
+    console.log("clear button pressed");
+    output.value = "";
+    numb = [];
+    oper = [];
+  } else
+  if (target.matches('.operator')) {
+    // console.log("operator button pressed", target.value);
+    if (!isNum && !target.matches('.minus') && !target.matches('.root')) {
+      console.log("cannot start with an operator or have two operators consecutively");
+      // alert('Cannot have two operators consecutively')
+    } else {
+      // ensure next input is a number
+      isNum = false;
+
+      // determine how to add last values to the arrays, modfiying as necessary
+      let operation = target.value;
+      let outArr = output.value.split(" ")
+      numb.push(parseFloat(outArr[outArr.length-1]));
+      console.log(numb);
+      if (addSqrt) {
+        console.log('fixing shit');
+        numb.push(.5);
+        // oper.push("^");
+        addSqrt = false;
+      }
+      if (addNeg) {
+        numb[numb.length-2] *= -1
+        addNeg = false;
+      }
+      if (addDen) {
+        numb[numb.length-1] = 1/numb[numb.length-1];
+        addDen = false;
+      }
+      switch (operation) {
+        case "+":
+          oper.push("+");
+          break;
+        case "-":
+          addNeg = true;
+          oper.push("+");
+          break;
+        case "X":
+          oper.push("*");
+          break;
+        case "/":
+          addDen = true;
+          oper.push("*");
+          break;
+        case "^":
+          oper.push("^")
+          break;
+        case "&radic;":
+          console.log("RADICAL!");
+          addSqrt = true;
+          numb.splice(numb.length-1, 1);
+          oper.push("^")
+          break;
+        default:
+          break;
+      }
+    }
+    // console.log(numb, oper);
+    output.value += " " + event.target.value;
+  } else
+  if (target.matches('.equal')) {
+    let outArr = output.value.split(" ");
+    numb.push(parseFloat(outArr[outArr.length-1]));
+    if (addSqrt) {
+      numb.push(.5);
+      oper.push("^");
+      addSqrt = false;
+    }
+    if (addNeg) {
+      numb[numb.length-2] *= -1
+      addNeg = false;
+    }
+    if (addDen) {
+      numb[numb.length-1] = 1/numb[numb.length-1];
+      addDen = false;
+    }
+    console.log(numb, oper);
+    calc(numb, oper);
+    output.value = numb[0];
+    console.log(numb[0]);
+    numb = [], oper = [];
+  }
+}
 
 function rounding(number, precision) {
-  // blatently stolen from MDN article on Math.round
+  // taken from MDN article on Math.round()
   var factor = Math.pow(10, precision);
   var tempNumber = number * factor;
   var roundedTempNumber = Math.round(tempNumber);
