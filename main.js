c2c8e6510e34503bdb889af1ec9e6c8b88fbaa3b
Brad Broge <brbroge@gmail.com>
2017-06-13 09:05:53 -0400

    refactoring code and adding styling

--- main.js
+++ main.js
@@ -2,33 +2,10 @@
 // will only use the '^', '*', '%', and '+' operators
 // numbers will be adjusted with - (n*-1), / (1/n), and sqrt (n**.5);
 // keep track if input isNum or operator.
+// Deprecated logic when eval("function") was discovered.
 
 var numb = [], oper = [], out = "", isNum = true;
 var addSqrt = false, addNeg = false, addDen = false;
-
-// Adding a bunch of listeners for all the buttons
-// WAIT: May be able to use event Delegation to only have one listener on
-// the calc-body section, and work from there.
-// let clear = document.querySelector('.clear');
-// let equal = document.querySelector('.equal');
-// let plus = document.querySelector('.plus');
-// let minus = document.querySelector('.minus');
-// let multiply = document.querySelector('.multiply');
-// let divide = document.querySelector('.divide');
-// let raise = document.querySelector('.raise');
-// let root = document.querySelector('.root');
-// let dec = document.querySelector('.dec');
-// let mod = document.querySelector('.mod');
-// let one = document.querySelector('.one');
-// let two = document.querySelector('.two');
-// let three = document.querySelector('.three');
-// let four = document.querySelector('.four');
-// let five = document.querySelector('.five');
-// let six = document.querySelector('.six');
-// let seven = document.querySelector('.seven');
-// let eight = document.querySelector('.eight');
-// let nine = document.querySelector('.nine');
-// let zero = document.querySelector('.zero');
 
 let output = document.querySelector('.output');
 let calculator = document.querySelector('.calc-body');
@@ -43,8 +20,6 @@ function updateValues(event) {
       output.value += " ";
       isNum = true;
     }
-    // console.log(event.target);
-    // console.log("button pressed: ", target.value);
     output.value += event.target.value;
   } else
   if (target.matches('.clear')) {
@@ -57,82 +32,24 @@ function updateValues(event) {
     // console.log("operator button pressed", target.value);
     if (!isNum && !target.matches('.minus') && !target.matches('.root')) {
       console.log("cannot start with an operator or have two operators consecutively");
-      // alert('Cannot have two operators consecutively')
     } else {
       // ensure next input is a number
       isNum = false;
-
-      // determine how to add last values to the arrays, modfiying as necessary
       let operation = target.value;
-      let outArr = output.value.split(" ")
-      numb.push(parseFloat(outArr[outArr.length-1]));
-      console.log(numb);
-      if (addSqrt) {
-        console.log('fixing shit');
-        numb.push(.5);
-        // oper.push("^");
-        addSqrt = false;
       }
-      if (addNeg) {
-        numb[numb.length-2] *= -1
-        addNeg = false;
-      }
-      if (addDen) {
-        numb[numb.length-1] = 1/numb[numb.length-1];
-        addDen = false;
-      }
-      switch (operation) {
-        case "+":
-          oper.push("+");
-          break;
-        case "-":
-          addNeg = true;
-          oper.push("+");
-          break;
-        case "X":
-          oper.push("*");
-          break;
-        case "/":
-          addDen = true;
-          oper.push("*");
-          break;
-        case "^":
-          oper.push("^")
-          break;
-        case "&radic;":
-          console.log("RADICAL!");
-          addSqrt = true;
-          numb.splice(numb.length-1, 1);
-          oper.push("^")
-          break;
-        default:
-          break;
-      }
-    }
-    // console.log(numb, oper);
     output.value += " " + event.target.value;
   } else
   if (target.matches('.equal')) {
-    let outArr = output.value.split(" ");
-    numb.push(parseFloat(outArr[outArr.length-1]));
-    if (addSqrt) {
-      numb.push(.5);
-      oper.push("^");
-      addSqrt = false;
+
+    console.log(output.value);
+    let total = output.value;
+    if (total.indexOf("X") > -1) {
+      total = total.replace(/X/g , "*");
+    } if (total.indexOf("^") > -1) {
+      total = total.replace(/\^/g , "**");
     }
-    if (addNeg) {
-      numb[numb.length-2] *= -1
-      addNeg = false;
-    }
-    if (addDen) {
-      numb[numb.length-1] = 1/numb[numb.length-1];
-      addDen = false;
-    }
-    console.log(numb, oper);
-    calc(numb, oper);
-    output.value = numb[0];
-    console.log(numb[0]);
-    numb = [], oper = [];
+    console.log(total)
+    output.value = eval(total);
   }
 }
 
